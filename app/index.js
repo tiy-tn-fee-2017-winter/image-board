a13af656d6213cd2e9f69917768fe336c528b584
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-10 09:03:43 -0600

    04 - Form Toggle in JS

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
 import 'whatwg-fetch';
+
+const form = document.querySelector('.top-form');
+const navButton = document.querySelector('.form-toggle');
+
+navButton.addEventListener('click', () => {
+  navButton.querySelector('.form-toggle__icon')
+    .classList.toggle('active');
+  form.classList.toggle('top-form--active');
+});
