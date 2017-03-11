461837a15d9e28ff40d235d921204afaa29a2c45
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-11 12:30:37 -0600

    05 - Submitting Form with POST request

--- app/index.js
+++ app/index.js
@@ -8,3 +8,28 @@ navButton.addEventListener('click', () => {
     .classList.toggle('active');
   form.classList.toggle('top-form--active');
 });
+
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+
+  const urlInput = document.querySelector('[name=url]');
+  const captionInput = document.querySelector('[name=caption]');
+
+  const data = {
+    url: urlInput.value,
+    caption: captionInput.value,
+  };
+
+  fetch('https://tiny-tn.herokuapp.com/collections/image-board-rt', {
+    method: 'POST',
+    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  navButton.querySelector('.form-toggle__icon')
+    .classList.toggle('active');
+  form.classList.toggle('top-form--active');
+
+  urlInput.value = '';
+  captionInput.value = '';
+});
