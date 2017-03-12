a98a1194e4bc22efe1e25ac7b3d465a63f3fd01d
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-11 18:58:25 -0600

    10 - Functional Refactor

--- app/index.js
+++ app/index.js
@@ -18,6 +18,23 @@ function toggleForm() {
   form.classList.toggle('top-form--active');
 }
 
+function createPostElement(post) {
+  const postEl = document.createElement('div');
+  postEl.classList.add('grid__item');
+
+  postEl.innerHTML = `
+    <div class="card">
+      <img src="" alt="" class="card__pic" />
+      <h2 class="card__caption"></h2>
+    </div>`;
+
+  postEl.querySelector('.card__pic').src = post.url;
+  postEl.querySelector('.card__pic').alt = post.caption;
+  postEl.querySelector('.card__caption').innerText = post.caption;
+
+  return postEl;
+}
+
 navButton.addEventListener('click', toggleForm);
 cancelButton.addEventListener('click', () => {
   toggleForm();
@@ -38,18 +55,7 @@ form.addEventListener('submit', (ev) => {
     body: JSON.stringify(data),
   }).then(res => res.json())
     .then((post) => {
-      const postEl = document.createElement('div');
-      postEl.classList.add('grid__item');
-
-      postEl.innerHTML = `
-        <div class="card">
-          <img src="" alt="" class="card__pic" />
-          <h2 class="card__caption"></h2>
-        </div>`;
-
-      postEl.querySelector('.card__pic').src = post.url;
-      postEl.querySelector('.card__pic').alt = post.caption;
-      postEl.querySelector('.card__caption').innerText = post.caption;
+      const postEl = createPostElement(post);
 
       gridEl.insertBefore(postEl, gridEl.querySelector('.grid__item'));
     });
@@ -61,24 +67,8 @@ form.addEventListener('submit', (ev) => {
 fetch('https://tiny-tn.herokuapp.com/collections/image-board-rt')
   .then(res => res.json())
   .then((data) => {
-    for (let i = 0; i < data.length; i++) {
-      const item = data[i];
-
-      const itemEl = document.createElement('div');
-      itemEl.classList.add('grid__item');
-      itemEl.innerHTML = `
-        <div class="card">
-          <img src="" alt="" class="card__pic">
-          <h2 class="card__caption"></h2>
-        </div>`;
-
-      const imgEl = itemEl.querySelector('.card__pic');
-      const captionEl = itemEl.querySelector('.card__caption');
-
-      imgEl.src = item.url;
-      imgEl.alt = item.caption;
-      captionEl.innerText = item.caption;
-
-      gridEl.appendChild(itemEl);
-    }
+    data.map(createPostElement)
+      .forEach((postEl) => {
+        gridEl.appendChild(postEl);
+      });
   });
