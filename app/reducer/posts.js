b8ed8fb3f70ed2875b2265a0657679d1c068dc88
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-11 19:23:07 -0600

    14 - TDD Redux Basic Findall

--- app/reducer/posts.js
+++ app/reducer/posts.js
@@ -1,5 +1,7 @@
 export default function posts(state = [], action) {
   switch (action.type) {
+    case 'POST@FINDALL_COMPLETE':
+      return [...action.data, ...state];
     case 'POST@CREATE_COMPLETE':
       return [action.data, ...state];
     default:
--- tests/reducer/posts-test.js
+++ tests/reducer/posts-test.js
@@ -24,4 +24,20 @@ module('reducer/posts', () => {
 
     assert.deepEqual(posts(oldState, action), expectedState);
   });
+
+  test('POST@FINDALL_COMPLETE: it can findall with an empty state', (assert) => {
+    const oldState = [];
+    const action = { type: 'POST@FINDALL_COMPLETE', data: [{ id: 2, url: 'http://placecage.com/200/200', caption: 'Cage' }] };
+    const expectedState = [{ id: 2, url: 'http://placecage.com/200/200', caption: 'Cage' }];
+
+    assert.deepEqual(posts(oldState, action), expectedState);
+  });
+
+  test('POST@FINDALL_COMPLETE: it can findall with a non-empty state', (assert) => {
+    const oldState = [{ id: 1, url: 'http://placecera.com/200/200', caption: 'Cera' }];
+    const action = { type: 'POST@FINDALL_COMPLETE', data: [{ id: 2, url: 'http://placecage.com/200/200', caption: 'Cage' }] };
+    const expectedState = [{ id: 2, url: 'http://placecage.com/200/200', caption: 'Cage' }, { id: 1, url: 'http://placecera.com/200/200', caption: 'Cera' }];
+
+    assert.deepEqual(posts(oldState, action), expectedState);
+  });
 });
