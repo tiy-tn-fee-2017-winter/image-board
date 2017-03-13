773d9afdf5ca81bb5d7810cf98c4f70544cc97d7
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-13 07:45:56 -0500

    27 - Vue form submit and clear

--- app/components/card-form.vue
+++ app/components/card-form.vue
@@ -7,14 +7,14 @@
         </button>
       </div>
     </header>
-    <form class="top-form" v-bind:class="{ 'top-form--active': active }">
+    <form class="top-form" @submit.prevent="save" v-bind:class="{ 'top-form--active': active }">
       <div class="container">
         <div class="form-control">
-          <input type="text" class="form-control__input" name="url" placeholder="Image URL">
+          <input v-model="newPost.url" type="text" class="form-control__input" name="url" placeholder="Image URL">
         </div>
 
         <div class="form-control">
-          <input type="text" class="form-control__input" name="caption" placeholder="Image Caption">
+          <input v-model="newPost.caption" type="text" class="form-control__input" name="caption" placeholder="Image Caption">
         </div>
 
         <div class="form-actions">
@@ -27,18 +27,40 @@
 </template>
 
 <script>
+import { create } from '../actions/post';
+import store from '../store';
+
 export default {
   name: 'CardForm',
+
   data() {
     return {
-      active: false
+      active: false,
+      newPost: {
+        url: '',
+        caption: ''
+      }
     };
   },
 
   methods: {
     toggleForm() {
       this.active = !this.active;
-    }
+    },
+
+    clearForm() {
+      this.newPost = {
+        url: '',
+        caption: '',
+      };
+    },
+
+    save() {
+      store.dispatch(create(this.newPost));
+
+      this.toggleForm();
+      this.clearForm();
+    },
   },
 };
 </script>
