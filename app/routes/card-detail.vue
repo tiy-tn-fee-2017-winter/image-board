7feac201345d057299d00a938b76959358deea33
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-14 08:00:04 -0500

    32 - Vue Detail HTML

--- app/routes/card-detail.vue
+++ app/routes/card-detail.vue
@@ -1,7 +1,21 @@
 <template lang="html">
   <div class="grid">
     <div class="grid__item">
-      <h2>Detail</h2>
+      <form class="card">
+        <h2>Editing --Photo Caption Here--</h2>
+        <div class="form-control">
+          <input type="text" class="form-control__input">
+        </div>
+        <div class="form-control">
+          <input type="text" class="form-control__input">
+        </div>
+
+        <div class="form-actions">
+          <router-link v-bind:to="{ name: 'index' }" class="button button--cancel">Back</router-link>
+          <button class="button button--danger">Delete</button>
+          <button class="button button--success">Save</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
